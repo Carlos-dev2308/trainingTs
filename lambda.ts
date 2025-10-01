653b4d1be3ad56a3c108cc414423887a140268be
Carlos-dev2308 <carlosenmanuel2308@gmail.com>
2025-10-01 12:13:41 -0400

    Update lambda.ts

--- lambda.ts
+++ lambda.ts
@@ -9,3 +9,18 @@ const listaProducto=[
 const productoRopa= listaProducto.filter((producto)=>producto.tipo==="ropa").map(producto=>producto.precio*1.10).reduce((acumulator,currentValue)=>acumulator+currentValue,0);
 console.log(productoRopa);
 
+type Producto=[string,number,'agotado'|'en stock'];
+
+const lista:Producto[] =[
+    ["camisa", 50,'en stock'],
+    ['pantalon',20,'agotado'],
+    ['chaqueta',60,'en stock'],
+    ['coca-cola',5,'en stock'],
+    ['guante',10,'agotado']
+]
+
+let stock=(lista.sort((a,b)=>a[1]-b[1]).filter(producto=>producto[2]==='en stock').map(producto=>producto[0]+" "+producto[1]+'$'));
+console.log(stock);
+
+
+
